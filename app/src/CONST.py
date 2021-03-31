3e3ef3e11de70309ef81d70144b5f873adb2ac5f
Louis <paganin.louis@gmail.com>
2021-03-31 15:53:44 -0700

    nginx up to an api manager (auth, limi requests)

--- app/src/CONST.py
+++ app/src/CONST.py
@@ -15,6 +15,12 @@ REPO_PATH = ""
 #IP adress and port to connect to the API
 ADRESS = '0.0.0.0'
 PORT = 5000
-ADRESS_TEST = 'http://172.18.0.5:80'
-#AWS 
+#ADRESS_TEST = 'http://filrouge.louis.p2021.ajoga.fr:80'
+#ADRESS_TEST = 'http://6hsidkopo9.execute-api.us-east-1.amazonaws.com/starting:80'
+ADRESS_TEST = 'http://127.0.0.1:80'
+#ADRESS_TEST = 'https://filrouge-gate-away.louis.p2021.ajoga.fr:443'
+#ADRESS_TEST =  'https://filrougeapi.louis.p2021.ajoga.fr'
+#ADRESS_TEST =  'https://x9j885x924.execute-api.us-east-1.amazonaws.com/prod'
+#ADRESS_TEST =  'https://apifilrouge.louis.p2021.ajoga.fr'
+#AWS
 BUCKET_NAME = 'filrougestorage'
--- app/src/myTests.py
+++ app/src/myTests.py
@@ -3,24 +3,25 @@ import json
 import unittest
 import CONST
 from PyPDF2 import PdfFileReader
+from requests.auth import HTTPBasicAuth
 
 #TEST_FILES_PATH = CURRENT_DIRECTORY + "/../testFiles/"
-
+auth = HTTPBasicAuth('louis', 'paganin')
 
 textfile = {"file" : open("/home/louis/filRouge/projetFilRouge/testFiles/testtext.txt", "r")}
-r=requests.post(CONST.ADRESS_TEST + "/upload", files = textfile)
+r=requests.post(CONST.ADRESS_TEST + '/upload', auth=auth, files = textfile)
 r = r.json()
 print(r)
 
 textfile = {"file" : open("/home/louis/filRouge/projetFilRouge/testFiles/.txt", "r")}
 r=requests.post(CONST.ADRESS_TEST + "/upload", files = textfile)
 r = r.json()
-print(r)
+#print(r)
 
 pdf = {"file" : open("/home/louis/filRouge/projetFilRouge/testFiles/testpdf.pdf", "rb") }
 r=requests.post(CONST.ADRESS_TEST + "/upload", files = pdf)
 r = r.json()
-print(r)
+#print(r)
 
 csv = {"file" : open("/home/louis/filRouge/projetFilRouge/testFiles/airtravel.csv", encoding='utf-8') }
 r=requests.post(CONST.ADRESS_TEST + "/upload", files = csv)
